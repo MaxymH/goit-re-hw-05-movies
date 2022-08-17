5d715d067e239b2a8bf89a47ba0520cbcda35757
MaxymH <95626499+MaxymH@users.noreply.github.com>
2022-08-17 11:33:29 +0000

    Deploying to gh-pages from @ MaxymH/goit-re-hw-05-movies@bdc72ec62059c3adedf04815f57be0e769bf8947 🚀

---
+++ static/js/NotFound.a91a35fa.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[187],{9805:function(e,t,a){a.r(t),a.d(t,{default:function(){return r}});var n=a(184);function r(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)("img",{src:"https://seranking.com/blog/wp-content/uploads/2021/01/404_01-min.jpg",alt:"404"}),(0,n.jsx)("img",{src:"https://www.meme-arsenal.com/memes/c9e6371faa3b57eaee1d35595ca8e910.jpg",alt:"404"})]})}}}]);
+//# sourceMappingURL=NotFound.a91a35fa.chunk.js.map
